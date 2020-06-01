d0e447e764f1835205653d61649c391e3cef738d
YutaNishizuni <65146542+YutaNishizuni@users.noreply.github.com>
2020-06-01 15:26:54 +0900

    Add files via upload

---
+++ Haikei.cpp
@@ -0,0 +1,24 @@
+#include"DxLib.h"
+#include"Main.h"
+#include"Haikei.h"
+
+
+
+void HaikeiInit(void)
+{
+	
+	
+	
+	
+}
+void HaikeiMain(void)
+{
+
+}
+void HaikeiDraw(void)
+{
+	
+	
+
+	//SCREEN_SIZE_X/2,SCREEN_SIZE_Y/2
+}
