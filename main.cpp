a30fbd2a00c3ca188cac98c83c5da83d7cd4808c
YutaNishizuni <65146542+YutaNishizuni@users.noreply.github.com>
2020-06-05 15:29:56 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -39,6 +39,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int hosi3 = LoadGraph("img/hosi3.png");
 	int hosi4 = LoadGraph("img/hosi4.png");
 	int hosi5 = LoadGraph("img/hosi5.png");
+	int hosi6 = LoadGraph("img/hosi6.png");
+	int hosi7 = LoadGraph("img/hosi7.png");
+	int hosi8 = LoadGraph("img/hosi8.png");
+	int hosi9 = LoadGraph("img/hosi9.png");
+	int hosi10 = LoadGraph("img/hosi10.png");
+	int hosi11 = LoadGraph("img/hosi11.png");
+	int hosi12 = LoadGraph("img/hosi12.png");
+	int hosi13 = LoadGraph("img/hosi13.png");
+	int hosi14 = LoadGraph("img/hosi14.png");
+	int hosi15 = LoadGraph("img/hosi15.png");
+	int hosi16 = LoadGraph("img/hosi16.png");
+	int hosi17 = LoadGraph("img/hosi17.png");
+	int hosi18 = LoadGraph("img/hosi18.png");
+	int hosi19 = LoadGraph("img/hosi19.png");
+	int hosi20 = LoadGraph("img/hosi20.png");
+	int hosi21 = LoadGraph("img/hosi21.png");
+	int hosi22 = LoadGraph("img/hosi22.png");
+	int hosi23 = LoadGraph("img/hosi23.png");
+	int hosi24 = LoadGraph("img/hosi24.png");
+	int hosi25 = LoadGraph("img/hosi25.png");
+	int hosi26 = LoadGraph("img/hosi26.png");
+	int hosi27 = LoadGraph("img/hosi27.png");
+	int hosi28 = LoadGraph("img/hosi28.png");
+	int hosi29 = LoadGraph("img/hosi29.png");
+	int hosi30 = LoadGraph("img/hosi30.png");
+	int hosi31 = LoadGraph("img/hosi31.png");
+	int hosi32 = LoadGraph("img/hosi32.png");
+	
 //////////////////////////////////////////////////////////////
 	//bgm
 	int bgm1 = LoadSoundMem("sund/oke_song_kei_vegalost.mp3");
@@ -116,7 +144,39 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	bool isDebugMode = false;
 	int skyy = 0;
 	int skyy2 = 0;
-	int skyy3 = 0.0f;
+	int skyy3 = 0;
+	int skyy4 = 0;
+	int skyy5 = 0;
+	int skyy6 = 0;
+	int skyy7 = 0;
+	int skyy8 = 0;
+	int skyy9 = 0;
+	int skyy10 = 0;
+	int skyy11 = 0;
+	int skyy12 = 0;
+	int skyy13 = 0;
+	int skyy14 = 0;
+	int skyy15 = 0; 
+	int skyy16 = 0;
+	int skyy17 = 0;
+	int skyy18 = 0;
+	int skyy19 = 0;
+	int skyy20 = 0;
+	int skyy21 = 0;
+	int skyy22 = 0;
+	int skyy23 = 0;
+	int skyy24 = 0;
+	int skyy25 = 0;
+	int skyy26 = 0;
+	int skyy27 = 0;
+	int skyy28 = 0;
+	int skyy29 = 0;
+	int skyy30 = 0;
+	int skyy31 = 0;
+	int skyy32 = 0;
+
+
+
 	int bgidx = 0;
 
 
@@ -142,6 +202,63 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int py3 = px3;
 	int px4 = 50;
 	int py4 = px4;
+	int px5 = 50;
+	int py5 = px5;
+	int px6 = 50;
+	int py6 = px6;
+	int px7 = 50;
+	int py7 = px7;
+	int px8 = 50;
+	int py8 = px8;
+	int px9 = 50;
+	int py9 = px9;
+	int px10 = 50;
+	int py10 = px10;
+	int px11 = 50;
+	int py11 = px11;
+	int px12 = 50;
+	int py12 = px12;
+	int px13 = 50;
+	int py13 = px13;
+	int px14 = 50;
+	int py14 = px14;
+	int px15 = 50;
+	int py15 = px15;
+	int px16 = 50;
+	int py16 = px16;
+	int px17 = 50;
+	int py17 = px17;
+	int px18 = 50;
+	int py18 = px18;
+	int px19 = 50;
+	int py19 = px19;
+	int px20 = 50;
+	int py20 = px20;
+	int px21 = 50;
+	int py21 = px21;
+	int px22 = 50;
+	int py22 = px22;
+	int px23 = 50;
+	int py23 = px23;
+	int px24 = 50;
+	int py24 = px24;
+	int px25 = 50;
+	int py25 = px25;
+	int px26 = 50;
+	int py26 = px26;
+	int px27 = 50;
+	int py27 = px27;
+	int px28 = 50;
+	int py28 = px28;
+	int px29 = 50;
+	int py29 = px29;
+	int px30 = 50;
+	int py30 = px30;
+	int px31 = 50;
+	int py31 = px31;
+	int px32 = 50;
+	int py32 = px32;
+	
 	//int pos = 0;
 	int hosisyokix;
 	int hosiposx;
@@ -155,6 +272,61 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int hosiposy3;
 	int hosiposx4;
 	int hosiposy4;
+	int hosiposx5;
+	int hosiposy5;
+	int hosiposx6;
+	int hosiposy6;
+	int hosiposx7;
+	int hosiposy7;
+	int hosiposx8;
+	int hosiposy8;
+	int hosiposx9;
+	int hosiposy9;
+	int hosiposx10;
+	int hosiposy10;
+	int hosiposx11;
+	int hosiposy11;
+	int hosiposx12;
+	int hosiposy12;
+	int hosiposx13;
+	int hosiposy13;
+	int hosiposx14;
+	int hosiposy14;
+	int hosiposx15;
+	int hosiposy15;
+	int hosiposx16;
+	int hosiposy16;
+	int hosiposx17;
+	int hosiposy17;
+	int hosiposx18;
+	int hosiposy18;
+	int hosiposx19;
+	int hosiposy19;
+	int hosiposx20;
+	int hosiposy20;
+	int hosiposx21;
+	int hosiposy21;
+	int hosiposx22;
+	int hosiposy22;
+	int hosiposx23;
+	int hosiposy23;
+	int hosiposx24;
+	int hosiposy24;
+	int hosiposx25;
+	int hosiposy25;
+	int hosiposx26;
+	int hosiposy26;
+	int hosiposx27;
+	int hosiposy27;
+	int hosiposx28;
+	int hosiposy28;
+	int hosiposx29;
+	int hosiposy29;
+	int hosiposx30;
+	int hosiposy30;
+	int hosiposx31;
+	int hosiposy31;
+	
 	//float kousinnflg;
 ////////////////////////////////////////////////////////
 	while (ProcessMessage() == 0) {
@@ -179,8 +351,97 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		skyy = (skyy + 5) % 640;
 		skyy2 = (skyy2 + 2) % 640;
 		skyy3 = (skyy3 + 1) % 1040;
-		
+		skyy4 = (skyy4 + 5) % 640;
+		skyy5 = (skyy5 + 5) % 640;
+		skyy6 = (skyy6 + 5) % 640;
+		skyy7 = (skyy7 + 5) % 640;
+		skyy8 = (skyy8 + 5) % 640;
+		skyy9 = (skyy9 + 5) % 640;
+		skyy10 = (skyy10 + 5) % 640;
+		skyy11 = (skyy11 + 5) % 640;
+		skyy12 = (skyy12 + 5) % 640;
+		skyy13 = (skyy13 + 5) % 640;
+		skyy14 = (skyy14 + 5) % 640;
+		skyy15 = (skyy15 + 5) % 640;
+		skyy16 = (skyy16 + 5) % 640;
+		skyy17 = (skyy17 + 5) % 640;
+		skyy18 = (skyy18 + 5) % 640;
+		skyy19 = (skyy19 + 5) % 640;
+		skyy20 = (skyy20 + 5) % 640;
+		skyy21 = (skyy21 + 5) % 640;
+		skyy22 = (skyy22 + 5) % 640;
+		skyy23 = (skyy23 + 5) % 640;
+		skyy24 = (skyy24 + 5) % 640;
+		skyy25 = (skyy25 + 5) % 640;
+		skyy26 = (skyy26 + 5) % 640;
+		skyy27 = (skyy27 + 5) % 640;
+		skyy28 = (skyy28 + 5) % 640;
+		skyy29 = (skyy29 + 5) % 640;
+		skyy30 = (skyy30 + 5) % 640;
+		skyy31 = (skyy31 + 5) % 640;
+		skyy32 = (skyy32 + 5) % 640;
 
+
+
+		hosiposx3=0;
+		hosiposy3=0;
+		hosiposx4=10;
+		hosiposy4=0;
+		hosiposx5=20;
+		hosiposy5=-10;
+		hosiposx6=30;
+		hosiposy6=-20;
+		hosiposx7=30;
+		hosiposy7=0;
+		hosiposx8=40;
+		hosiposy8=0;
+		hosiposx9=50;
+		hosiposy9=-10;
+		hosiposx10=60;
+		hosiposy10=0;
+		hosiposx11=70;
+		hosiposy11=0;
+		hosiposx12=80;
+		hosiposy12=0;
+		hosiposx13=90;
+		hosiposy13=0;
+		hosiposx14=90;
+		hosiposy14=0;
+		hosiposx15=100;
+		hosiposy15=0;
+		hosiposx16=120;
+		hosiposy16=0;
+		hosiposx17=130;
+		hosiposy17=0;
+		hosiposx18=140;
+		hosiposy18=0;
+		hosiposx19=150;
+		hosiposy19=0;
+		hosiposx20=160;
+		hosiposy20=0;
+		hosiposx21=10;
+		hosiposy21=0;
+		hosiposx22=180;
+		hosiposy22=0;
+		hosiposx23=190;
+		hosiposy23=0;
+		hosiposx24=200;
+		hosiposy24=0;
+		hosiposx25=230;
+		hosiposy25=0;
+		hosiposx26=240;
+		hosiposy26=0;
+		hosiposx27=250;
+		hosiposy27=0;
+		hosiposx28=260;
+		hosiposy28=0;
+		hosiposx29=270;
+		hosiposy29=0;
+		hosiposx30=280;
+		hosiposy30=0;
+		hosiposx31=290;
+		hosiposy31=0;
+		
 		/////////////////////////////////////////////////////////////////////////
 		//îwåi
 		DrawGraph(0, 0, haikei, true);
@@ -220,7 +481,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 
-		DrawExtendGraph(hosiposx4 + px4, hosiposy4 + py4 + skyy, hosiposx4 + (px4 * 2), hosiposy4 + (py4 * 2) + skyy, hosi5, true);
+		DrawExtendGraph(hosiposx4 + px4, hosiposy4 + py4 + skyy5, hosiposx4 + (px4 * 2), hosiposy4 + (py4 * 2) + skyy5, hosi5, true);
 
 			
 			px1 = px1 + 1;
@@ -234,6 +495,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				px1 = 0;
 			}
 		
+
+			DrawExtendGraph(hosiposx5 + px5, hosiposy5 + py5 + skyy6, hosiposx5 + (px5 * 2), hosiposy5 + (py5 * 2) + skyy6, hosi6, true);
+			DrawExtendGraph(hosiposx6 + px6, hosiposy6 + py6 + skyy7, hosiposx6 + (px6 * 2), hosiposy6 + (py6 * 2) + skyy7, hosi7, true);
+			DrawExtendGraph(hosiposx7 + px7, hosiposy7 + py7 + skyy8, hosiposx7 + (px7 * 2), hosiposy7 + (py7 * 2) + skyy8, hosi8, true);
+			DrawExtendGraph(hosiposx8 + px8, hosiposy8 + py8 + skyy9, hosiposx8 + (px8 * 2), hosiposy8 + (py8 * 2) + skyy9, hosi9, true);
+			DrawExtendGraph(hosiposx9 + px9, hosiposy9 + py9 + skyy10, hosiposx9 + (px9 * 2), hosiposy9 + (py9 * 2) + skyy10, hosi10, true);
+			DrawExtendGraph(hosiposx10 + px10, hosiposy10 + py10 + skyy11, hosiposx10 + (px10 * 2), hosiposy10 + (py10 * 2) + skyy11, hosi11, true);
+			DrawExtendGraph(hosiposx11 + px11, hosiposy11 + py11 + skyy12, hosiposx11 + (px11 * 2), hosiposy11 + (py11 * 2) + skyy12, hosi12, true);
+			DrawExtendGraph(hosiposx12 + px12, hosiposy12 + py12 + skyy13, hosiposx12 + (px12 * 2), hosiposy12 + (py12 * 2) + skyy13, hosi13, true);
+			DrawExtendGraph(hosiposx13 + px13, hosiposy13 + py13 + skyy14, hosiposx13 + (px13 * 2), hosiposy13 + (py13 * 2) + skyy14, hosi14, true);
+			DrawExtendGraph(hosiposx14 + px14, hosiposy14 + py14 + skyy15, hosiposx14 + (px14 * 2), hosiposy14 + (py14 * 2) + skyy15, hosi15, true);
+			DrawExtendGraph(hosiposx15 + px15, hosiposy15 + py15 + skyy16, hosiposx15 + (px15 * 2), hosiposy15 + (py15 * 2) + skyy16, hosi16, true);
+			DrawExtendGraph(hosiposx16 + px16, hosiposy16 + py16 + skyy17, hosiposx16 + (px16 * 2), hosiposy16 + (py16 * 2) + skyy17, hosi17, true);
+			DrawExtendGraph(hosiposx17 + px17, hosiposy17 + py17 + skyy18, hosiposx17 + (px17 * 2), hosiposy17 + (py17 * 2) + skyy18, hosi18, true); 
+			DrawExtendGraph(hosiposx18 + px18, hosiposy18 + py18 + skyy19, hosiposx18 + (px18 * 2), hosiposy18 + (py18 * 2) + skyy19, hosi19, true);
+			DrawExtendGraph(hosiposx19 + px19, hosiposy19 + py19 + skyy20, hosiposx19 + (px19 * 2), hosiposy19 + (py19 * 2) + skyy20, hosi20, true);
+			DrawExtendGraph(hosiposx20 + px20, hosiposy20 + py20 + skyy21, hosiposx20 + (px20 * 2), hosiposy20 + (py20 * 2) + skyy21, hosi21, true);
+			DrawExtendGraph(hosiposx21 + px21, hosiposy21 + py21 + skyy22, hosiposx21 + (px21 * 2), hosiposy21 + (py21 * 2) + skyy22, hosi22, true);
+			DrawExtendGraph(hosiposx22 + px21, hosiposy22 + py22 + skyy23, hosiposx22 + (px22 * 2), hosiposy22 + (py22 * 2) + skyy23, hosi23, true);
+			DrawExtendGraph(hosiposx23 + px23, hosiposy23 + py23 + skyy24, hosiposx23 + (px23 * 2), hosiposy23 + (py23 * 2) + skyy24, hosi24, true);
+			DrawExtendGraph(hosiposx24 + px24, hosiposy24 + py24 + skyy25, hosiposx24 + (px24 * 2), hosiposy24 + (py24 * 2) + skyy25, hosi25, true);
+			DrawExtendGraph(hosiposx25 + px25, hosiposy25 + py25 + skyy26, hosiposx25 + (px25 * 2), hosiposy25 + (py25 * 2) + skyy26, hosi26, true);
+			DrawExtendGraph(hosiposx26 + px26, hosiposy26 + py26 + skyy27, hosiposx26 + (px26 * 2), hosiposy26 + (py26 * 2) + skyy27, hosi27, true);
+			DrawExtendGraph(hosiposx27 + px27, hosiposy27 + py27 + skyy28, hosiposx27 + (px27 * 2), hosiposy27 + (py27 * 2) + skyy28, hosi28, true);
+			DrawExtendGraph(hosiposx28 + px28, hosiposy28 + py28 + skyy29, hosiposx28 + (px28 * 2), hosiposy28 + (py28 * 2) + skyy29, hosi29, true);
+			DrawExtendGraph(hosiposx29 + px29, hosiposy29 + py29 + skyy30, hosiposx29 + (px29 * 2), hosiposy29 + (py29 * 2) + skyy30, hosi30, true);
+			DrawExtendGraph(hosiposx30 + px30, hosiposy30 + py30 + skyy31, hosiposx30 + (px30 * 2), hosiposy30 + (py30 * 2) + skyy31, hosi31, true);
+		//	DrawExtendGraph(hosiposx31 + px31, hosiposy31 + py31 + skyy32, hosiposx31 + (px31 * 2), hosiposy31 + (py31 * 2) + skyy32, hosi32, true);
+	
 	//	DrawGraph(pos, pos, hosi2, true);
 
 ////////////////////////////////////////////////////////////////////////////////
